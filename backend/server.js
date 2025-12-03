b5d1dea1f3174a58cb41dacaa52ff4f4e72a9f72
Carol Rimoun <academiccarol@gmail.com>
2025-12-04 01:22:57 +0200

    deployment

--- backend/server.js
+++ backend/server.js
@@ -1,6 +1,8 @@
 import express from "express";
 import cors from "cors";
 import dotenv from "dotenv";
+import path from "path";
+import { fileURLToPath } from "url";
 
 import roomsRoute from "./src/routes/rooms.js";
 import testRoutes from "./src/routes/tests.routes.js";
@@ -18,6 +20,15 @@ app.use("/api/rooms", roomsRoute);
 app.use("/api/admin-rooms", adminRoomRoute);
 app.use("/api/test", testRoutes);
 app.use("/auth", authRoutes);   // mount auth
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+// Serve frontend
+app.use(express.static(path.join(__dirname, "../frontend/dist")));
+
+app.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname, "../frontend/dist/index.html"));
+});
 
 // root test endpoint
 app.get("/", (req, res) => res.send("University Management API is running."));
